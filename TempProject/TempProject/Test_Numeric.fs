0e1f7452b79238962d039649e6d79b2e8830047e
korytkoBT <korytko07@gmail.com>
2015-10-24 19:33:41 +0200

    TempProject Before Templating

---
+++ TempProject/TempProject/Test_Numeric.fs
@@ -0,0 +1,17 @@
+﻿module Test_Numeric
+
+open System
+open System.Collections
+open NUnit.Framework
+open NUnit.Framework.Constraints
+
+// ======== Lists 1.1 ============
+[<Test>]
+let TestOfAddingValues() =
+  let _inputVal1 = 0;
+  let _inputVal2 = 1;
+
+  let _expectedVal = 1;
+  let _reachedVal = Numeric.add(_inputVal1,_inputVal2)
+
+  Assert.AreEqual(_expectedVal,_reachedVal)
