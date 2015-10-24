d670c3a83355027fe902e8596d54ff0b6df477a9
korytkoBT <korytko07@gmail.com>
2015-10-24 22:12:18 +0200

    Installed Click And Go To Refference Extension

--- Porting_1_Exercise_List/Porting_1_Exercise_List/Test_List.fs
+++ Porting_1_Exercise_List/Porting_1_Exercise_List/Test_List.fs
@@ -4,15 +4,15 @@ open System
 open System.Collections
 open NUnit.Framework
 open NUnit.Framework.Constraints
-open MyNamespace
+open MyNamespace.Lists
 
 // ======== Lists 1.1 ============
 [<Test>]
-let TestOfReversingList() =
+let ListPalindromeWithRev() =
   let _inputVal1 = [1;2;3]
 
   let _expectedVal = [3;2;1]
-  let _reachedVal = Lists.palindrome _inputVal1
+  let _reachedVal = palindrome _inputVal1
 
   Assert.AreEqual(_expectedVal,_reachedVal)
 
@@ -21,7 +21,15 @@ let TestOfLengthList() =
   let _inputVal1 = [1;2;3]
 
   let _expectedVal = 3
-  let _reachedVal = Lists.length _inputVal1
+  let _reachedVal = length _inputVal1
 
   Assert.AreEqual(_expectedVal,_reachedVal)
 
+[<Test>]
+let TestOfReversingList() =
+  let _inputVal1 = [1;2;3]
+
+  let _expectedVal = [3;2;1]
+  let _reachedVal = palindrome _inputVal1
+
+  Assert.AreEqual(_expectedVal,_reachedVal)
